d056c2a917dddc4b9c9d9b937c26b23a35d190d5
Johnathan Hill <Johnathanbhill9@mail.fresnostate.edu>
2024-06-20 19:49:47 -0700

    Updates CityForecastHeader and Product Bundle Identifier

--- lib/pages/city_forecast/city_forecast.dart
+++ lib/pages/city_forecast/city_forecast.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/pages/city_forecast/widgets/city_header.dart';
 import 'package:weather_app/pages/city_forecast/widgets/feels_like.dart';
-import 'package:weather_app/pages/city_forecast/widgets/sunrise.dart';
+import 'package:weather_app/pages/city_forecast/widgets/forecast_hour_list/forecast_hour_list.dart';
+import 'package:weather_app/pages/city_forecast/widgets/future_forecast.dart';
 import 'package:weather_app/pages/city_forecast/widgets/humidity.dart';
+import 'package:weather_app/pages/city_forecast/widgets/next_hour.dart';
 import 'package:weather_app/pages/city_forecast/widgets/pressure.dart';
 import 'package:weather_app/pages/city_forecast/widgets/rainfall.dart';
+import 'package:weather_app/pages/city_forecast/widgets/sunrise.dart';
 import 'package:weather_app/pages/city_forecast/widgets/uv_index.dart';
-import 'package:weather_app/pages/city_forecast/widgets/next_hour.dart';
 import 'package:weather_app/pages/city_forecast/widgets/weather_visibility.dart';
 import 'package:weather_app/pages/city_forecast/widgets/wind.dart';
 import 'package:weather_app/theme/colors.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/utils/utils.dart';
-import 'package:weather_app/pages/city_forecast/widgets/city_header.dart';
-import 'package:weather_app/pages/city_forecast/widgets/forecast_hour_list/forecast_hour_list.dart';
-import 'package:weather_app/pages/city_forecast/widgets/future_forecast.dart';
 
 import 'cubit/forecast_cubit.dart';
 import 'widgets/air_quality.dart';
@@ -30,22 +30,20 @@ class CityForecast extends StatefulWidget {
   State<CityForecast> createState() => _CityForecast();
 }
 
-class _CityForecast extends State<CityForecast>
-    with SingleTickerProviderStateMixin {
+class _CityForecast extends State<CityForecast> with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
 
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 700));
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(milliseconds: 700));
 
     _animationController.forward();
   }
 
   @override
-  Widget build(BuildContext context) =>
-      BlocBuilder<ForecastCubit, ForecastState>(
+  Widget build(BuildContext context) => BlocBuilder<ForecastCubit, ForecastState>(
         builder: (context, state) {
           return Scaffold(
             backgroundColor: WeatherColors.black,
@@ -64,8 +62,7 @@ class _CityForecast extends State<CityForecast>
                               height: constraints.maxHeight - 340,
                               child: SingleChildScrollView(
                                 child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 15, vertical: 20),
+                                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
                                   child: Column(
                                     children: [
                                       const SizedBox(height: 5),
@@ -80,29 +77,18 @@ class _CityForecast extends State<CityForecast>
                                                 AnimatedPositioned(
                                                   left: 0,
                                                   right: 0,
-                                                  top:
-                                                      state.isLoading ? -75 : 0,
-                                                  duration: const Duration(
-                                                      milliseconds: 500),
+                                                  top: state.isLoading ? -75 : 0,
+                                                  duration: const Duration(milliseconds: 500),
                                                   child: AnimatedOpacity(
-                                                    opacity:
-                                                        state.isLoading ? 0 : 1,
-                                                    duration: const Duration(
-                                                        milliseconds: 1000),
+                                                    opacity: state.isLoading ? 0 : 1,
+                                                    duration: const Duration(milliseconds: 1000),
                                                     child: NextHour(
-                                                        isLoading:
-                                                            state.isLoading,
+                                                        isLoading: state.isLoading,
                                                         condition: state
-                                                                .forecast
-                                                                ?.current
-                                                                .condition
-                                                                .text ??
+                                                                .forecast?.current.condition.text ??
                                                             '',
                                                         iconAsset: state
-                                                                .forecast
-                                                                ?.current
-                                                                .condition
-                                                                .icon ??
+                                                                .forecast?.current.condition.icon ??
                                                             ''),
                                                   ),
                                                 ),
@@ -116,19 +102,13 @@ class _CityForecast extends State<CityForecast>
                                                 AnimatedPositioned(
                                                   left: 0,
                                                   right: 0,
-                                                  top:
-                                                      state.isLoading ? -75 : 0,
-                                                  duration: const Duration(
-                                                      milliseconds: 900),
+                                                  top: state.isLoading ? -75 : 0,
+                                                  duration: const Duration(milliseconds: 900),
                                                   child: AnimatedOpacity(
-                                                    opacity:
-                                                        state.isLoading ? 0 : 1,
-                                                    duration: const Duration(
-                                                        milliseconds: 1200),
+                                                    opacity: state.isLoading ? 0 : 1,
+                                                    duration: const Duration(milliseconds: 1200),
                                                     child: ForecastHourList(
-                                                        forecastHours: state
-                                                                .hourlyForecast ??
-                                                            []),
+                                                        forecastHours: state.hourlyForecast ?? []),
                                                   ),
                                                 ),
                                               ],
@@ -141,26 +121,20 @@ class _CityForecast extends State<CityForecast>
                                                 AnimatedPositioned(
                                                   left: 0,
                                                   right: 0,
-                                                  top:
-                                                      state.isLoading ? -75 : 0,
-                                                  duration: const Duration(
-                                                      milliseconds: 900),
+                                                  top: state.isLoading ? -75 : 0,
+                                                  duration: const Duration(milliseconds: 900),
                                                   child: AnimatedOpacity(
-                                                    opacity:
-                                                        state.isLoading ? 0 : 1,
-                                                    duration: const Duration(
-                                                        milliseconds: 1400),
+                                                    opacity: state.isLoading ? 0 : 1,
+                                                    duration: const Duration(milliseconds: 1400),
                                                     child: FutureForecast(
-                                                        forecastDays:
-                                                            state.forecastDays),
+                                                        forecastDays: state.forecastDays),
                                                   ),
                                                 ),
                                               ],
                                             ),
                                           ),
                                           AirQualityCard(
-                                            airQuality: state
-                                                .forecast?.current.airQuality,
+                                            airQuality: state.forecast?.current.airQuality,
                                           ),
                                           Row(
                                             children: [
@@ -185,12 +159,8 @@ class _CityForecast extends State<CityForecast>
                                             children: [
                                               Expanded(
                                                   child: Wind(
-                                                windSpeed: state.forecast
-                                                        ?.current.windMph ??
-                                                    0,
-                                                windDegree: state.forecast
-                                                        ?.current.windDegree ??
-                                                    0,
+                                                windSpeed: state.forecast?.current.windMph ?? 0,
+                                                windDegree: state.forecast?.current.windDegree ?? 0,
                                               )),
                                               const SizedBox(width: 10),
                                               const Expanded(child: Rainfall()),
@@ -200,19 +170,13 @@ class _CityForecast extends State<CityForecast>
                                             children: [
                                               Expanded(
                                                   child: FeelsLike(
-                                                feelsLike: state.forecast
-                                                        ?.current.feelsLikeF ??
-                                                    0,
-                                                currentTemp: state.forecast
-                                                        ?.current.tempF ??
-                                                    0,
+                                                feelsLike: state.forecast?.current.feelsLikeF ?? 0,
+                                                currentTemp: state.forecast?.current.tempF ?? 0,
                                               )),
                                               const SizedBox(width: 10),
                                               Expanded(
                                                   child: Humidity(
-                                                humidity: state.forecast
-                                                        ?.current.humidity ??
-                                                    0,
+                                                humidity: state.forecast?.current.humidity ?? 0,
                                               )),
                                             ],
                                           ),
@@ -220,18 +184,13 @@ class _CityForecast extends State<CityForecast>
                                             children: [
                                               Expanded(
                                                   child: WeatherVisibility(
-                                                visibility: state
-                                                        .forecast
-                                                        ?.current
-                                                        .visibilityMiles ??
-                                                    0,
+                                                visibility:
+                                                    state.forecast?.current.visibilityMiles ?? 0,
                                               )),
                                               const SizedBox(width: 10),
                                               Expanded(
                                                   child: Pressure(
-                                                pressure: state.forecast
-                                                        ?.current.pressureIn ??
-                                                    0,
+                                                pressure: state.forecast?.current.pressureIn ?? 0,
                                               )),
                                             ],
                                           )
@@ -245,7 +204,8 @@ class _CityForecast extends State<CityForecast>
                           ),
                           ClipRRect(
                             clipBehavior: Clip.hardEdge,
-                            borderRadius: BorderRadius.circular(40),
+                            borderRadius: const BorderRadius.only(
+                                bottomLeft: Radius.circular(40), bottomRight: Radius.circular(40)),
                             child: AnimatedSize(
                               alignment: Alignment.bottomCenter,
                               duration: const Duration(milliseconds: 750),
@@ -256,21 +216,15 @@ class _CityForecast extends State<CityForecast>
                                   child: CityHeader(
                                     isLoading: state.isLoading,
                                     title: state.forecast?.location.name ?? '',
-                                    condition: state
-                                            .forecast?.current.condition.text ??
-                                        '',
-                                    currentTemp:
-                                        '${state.forecast?.current.tempF.toInt() ?? ''}',
+                                    condition: state.forecast?.current.condition.text ?? '',
+                                    currentTemp: '${state.forecast?.current.tempF.toInt() ?? ''}',
                                     tempHi:
                                         '${state.forecastDays?.first.day.maxTempF.toInt() ?? ''}',
                                     tempLow:
                                         '${state.forecastDays?.first.day.minTempF.toInt() ?? ''}',
-                                    iconAsset: state
-                                            .forecast?.current.condition.icon ??
-                                        '',
+                                    iconAsset: state.forecast?.current.condition.icon ?? '',
                                     airQualityMessage: getAirQualityMessage(
-                                        usEpaIndex: state.forecast?.current
-                                            .airQuality.usEpaIndex),
+                                        usEpaIndex: state.forecast?.current.airQuality.usEpaIndex),
                                   ),
                                 ),
                               ),
