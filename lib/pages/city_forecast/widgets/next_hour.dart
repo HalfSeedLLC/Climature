db28c9e326829510779d9542dc1536f5278d5afc
Johnathan Hill <Johnathanbhill9@mail.fresnostate.edu>
2024-06-21 21:16:25 -0700

    Fixes some overflow issues

--- lib/pages/city_forecast/widgets/next_hour.dart
+++ lib/pages/city_forecast/widgets/next_hour.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:text_scroll/text_scroll.dart';
 import 'package:weather_app/widgets/gradient_container.dart';
 
 import '../../../theme/colors.dart';
@@ -17,64 +18,69 @@ class NextHour extends StatelessWidget {
   final bool isLoading;
 
   @override
-  Widget build(BuildContext context) => DecoratedBox(
-        decoration: BoxDecoration(
-            color: WeatherColors.ev1, borderRadius: BorderRadius.circular(20)),
-        child: Padding(
-          padding: const EdgeInsets.all(25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(width: 15),
-                  if (iconAsset.isNotEmpty)
-                    isLoading
-                        ? const GradientContainer(
-                            height: 45,
-                            width: 45,
-                            borderRadius: 45,
-                          )
-                        : SizedBox(
-                            width: 20,
-                            height: 45,
-                            child: OverflowBox(
-                              maxWidth: double.infinity,
-                              maxHeight: double.infinity,
-                              child: Image.asset(
-                                  width: 55,
-                                  height: 55,
-                                  getWeatherIconAsset(iconAsset: iconAsset)),
+  Widget build(BuildContext context) => LayoutBuilder(builder: (context, constraints) {
+        return DecoratedBox(
+          decoration:
+              BoxDecoration(color: WeatherColors.ev1, borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              children: [
+                if (iconAsset.isNotEmpty)
+                  isLoading
+                      ? const GradientContainer(
+                          height: 45,
+                          width: 45,
+                          borderRadius: 45,
+                        )
+                      : SizedBox(
+                          width: 45,
+                          height: 45,
+                          child: OverflowBox(
+                            maxWidth: double.infinity,
+                            maxHeight: double.infinity,
+                            child: Image.asset(
+                              width: 55,
+                              height: 55,
+                              getWeatherIconAsset(iconAsset: iconAsset),
                             ),
                           ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30),
-                    child: Wrap(
-                      spacing: 5,
-                      direction: Axis.vertical,
-                      children: [
-                        isLoading
-                            ? const GradientContainer(height: 20, width: 120)
-                            : Text(condition,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .labelLarge!
-                                    .copyWith(
-                                        height: 1,
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w500,
-                                        color: WeatherColors.white)),
-                        isLoading
-                            ? const GradientContainer(height: 20, width: 200)
-                            : Text('$condition for the next hour')
-                      ],
-                    ),
-                  )
-                ],
-              )
-            ],
+                        ),
+                const SizedBox(width: 10), // Add some space between icon and text
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      isLoading
+                          ? const GradientContainer(height: 20, width: 120)
+                          : Text(
+                              condition,
+                              style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                                  height: 1,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                  color: WeatherColors.white),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                      const SizedBox(height: 5), // Add some space between texts
+                      isLoading
+                          ? const GradientContainer(height: 20, width: 200)
+                          : TextScroll(
+                              '$condition for the next hour',
+                              mode: TextScrollMode.endless,
+                              velocity: const Velocity(pixelsPerSecond: Offset(25, 0)),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(height: 1, fontSize: 16, color: WeatherColors.white),
+                            ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      );
+        );
+      });
 }
